f3f8de89a84565329592b6a3da97862f7ec94190
Hermez Dalil <hermez.dalil@gmail.com>
2024-05-13 09:53:16 +0100

    recupération des email et numero de telephone depuis une liste desitesweb

--- email_phone_scrapper.py
+++ email_phone_scrapper.py
@@ -4,11 +4,14 @@ import requests
 
 allsite = ["https://www.systhen.com",
            "https://www.systhen.com/contact/", "https://www.zeendoc.com", "https://www.zeendoc.com/votre-projet/", "https://www.zeendoc.com/contact/", "https://www.zeendoc.com/prendre-rdv/",
-           "https://www.cotranet.com", "https://www.cotranet.com/rendez-vous/", "https://www.getyooz.com/fr/", "https://www.getyooz.com/fr/nous-contacter"]
+           "https://www.cotranet.com", "https://www.cotranet.com/rendez-vous/", "https://www.qualios.com/fr/contact.html","https://www.getyooz.com/fr/", "https://www.getyooz.com/fr/nous-contacter"
+           , "https://www.qualios.com/fr/presentation-qualios-doc.html", "https://www.laserfiche.com/contact/", "https://www.laserfiche.com"
+           , "https://www.dimo-dematerialisation.com/contact/", "https://www.dimo-dematerialisation.com/logiciel-ged/", "https://www.ged.fr/ged-contact/"
+           , "https://www.ged.fr/logiciel-ged/", "https://www.scoqi.fr/logiciel-qualite-scoqi-demande-de-documentation-demande-de-contact/", "https://www.scoqi.fr/logiciel-qualite/gestion-documentaire-ged-gestion-des-documents-logiciel-qualite-de-gestion-documentaire-ged/?gad_source=1&gclid=CjwKCAjw9IayBhBJEiwAVuc3fgJtfB65czRLPGDPrhfmCnAT2JLOVus6T0UPS9dY77hTo3iKObZ5tRoCA40QAvD_BwE"]
 emails = []
 tels = []
 for l in allsite:
-    print("le site " + l + " contient :")
+    print("----------------- le site " + l + " contient : ----------------------")
     r = requests.get(l)
     soup = BeautifulSoup(r.content, "html.parser")
     for link in soup.findAll('a', attrs={'href': re.compile("^mailto:")}):
