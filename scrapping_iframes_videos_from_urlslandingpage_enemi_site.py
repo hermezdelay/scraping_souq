feefd624ab6b51875562ce73ade16430c3398c23
Hermez Dalil <hermez.dalil@gmail.com>
2024-05-14 09:02:59 +0100

    recupération des urls de videos dans la landing page du site des anime www.anume4up.com

--- scrapping_iframes_videos_from_urlslandingpage_enemi_site.py
+++ scrapping_iframes_videos_from_urlslandingpage_enemi_site.py
@@ -9,11 +9,11 @@ for l in site:
     soup = BeautifulSoup(r.content, "html.parser")
     for link in soup.findAll('a', attrs={'href': re.compile("^https:")}):
         allsite.append(link.get('href'))
-print(allsite)
+print("all sites are here : " + str(allsite))
 for l in allsite:
     r = requests.get(l)
     soup = BeautifulSoup(r.content, "html.parser")
     for link in soup.findAll('iframe', attrs={}):
         linkes.append(link.get('src'))
-        print(link.get('src'))
+        print("Links here : " + str(link.get('src')))
 print("Finsihed")
