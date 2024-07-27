1ff4167e3155ec7c8e27503789dbdb504e8358f4
dalil16 <hermez.dalil@gmail.com>
2024-07-27 19:32:36 +0100

    ajout de make it in germany

--- make_it_in_germany/make_it_germany_scrapper.py
+++ make_it_in_germany/make_it_germany_scrapper.py
@@ -9,32 +9,40 @@ import csv
 
 allsite = ["https://www.make-it-in-germany.com/fr/travailler-en-allemagne/bourse-de-lemploi?tx_solr%5Bfilter%5D%5B0%5D=topjobs%3A4&tx_solr%5Bpage%5D=",
            "&tx_solr%5Bq%5D=web#list45538"]
+sites = []
 emails = []
 tels = []
 
 filecsv = open('make_it_in_germany.csv', 'w', encoding='utf8')
-# Set the URL you want to webscrape from
-url = 'https://saudi.souq.com/sa-ar/apple/new/a-c/s/?section=2&page='
 file = open('SouqDataapple.json', 'w', encoding='utf8')
 file.write('[\n')
 data = {}
 csv_columns = ['name', 'price', 'img']
 
-
-for page in range(1000):
+# je recherche dans les pages tous les url qui contienent la description du job
+for page in range(10):
     print('---', page, '---')
     r = requests.get(allsite[0] + str(page) + allsite[1] )
     soup = BeautifulSoup(r.content, "html.parser")
-    for link in soup.findAll('a', attrs={'class':'btn btn--primary'}):
+    for link in soup.findAll('a', attrs={'class':'btn btn--primary', 'href': re.compile("^https:")}):
+        sites.append(link.get('href'))
+        print(link.get('href'))
+
+for l in sites:
+    print("----------------- le site " + l + " contient : ----------------------")
+    r = requests.get(l)
+    soup = BeautifulSoup(r.content, "html.parser")
+    for link in soup.findAll('a', attrs={'href': re.compile("^mailto:")}):
         emails.append(link.get('href'))
         print(link.get('href'))
-    for link in soup.findAll('a', attrs={'href': re.compile("^mailto:")}):
+
+    """for link in soup.findAll('a', attrs={'href': re.compile("^mailto:")}):
         emails.append(link.get('href'))
         print(link.get('href'))
     for tel in soup.findAll('a', attrs={'href': re.compile("^tel:")}):
         tels.append(tel.get('href'))
         print(tel.get('href'))
-
+    """
 print("Toute la liste des emails est ici : ")
 print(emails)
 print(tels)
